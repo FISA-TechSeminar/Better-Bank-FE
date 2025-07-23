b91a1d9fc67ced87caf35dba4b1dcc09b03c0ce5
0-zoo <dldl0010987@gmail.com>
2025-07-23 19:53:58 +0900

    [#6] 이자 받기 성공 UI

---
+++ the-better-bank/src/components/transaction/InterestReceived.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import './InterestReceived.css';
+import gifImage from '../../assets/Wallet.gif'; 
+
+export default function InterestReceived() {
+  return (
+    <div className="interest-received-container">
+      {/* GIF 애니메이션 */}
+      <img src={gifImage} alt="이자 수령 애니메이션" className="gif" />
+
+      {/* 텍스트 */}
+      <div className="text">
+        <div className="main-text">돈이 담긴 통장 도착!</div>
+      </div>
+
+      {/* 닫기 버튼 */}
+      <button className="close-button" onClick={() => window.history.back()}>
+        닫기
+      </button>
+    </div>
+  );
+}
+
